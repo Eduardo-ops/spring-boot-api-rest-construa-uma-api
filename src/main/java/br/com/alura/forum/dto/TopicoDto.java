cbda94920f3fa9af83cf7181cb5376271403c89b
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2022-08-15 11:11:57 -0300

    Implementado a usabilidade de DTO

---
+++ src/main/java/br/com/alura/forum/dto/TopicoDto.java
@@ -0,0 +1,49 @@
+package br.com.alura.forum.dto;
+
+import br.com.alura.forum.model.Topico;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TopicoDto {
+
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataDeCriacao;
+
+    public TopicoDto(Topico topico) {
+        this.titulo = topico.getTitulo();
+        this.mensagem = topico.getMensagem();
+        this.dataDeCriacao = topico.getDataDeCriacao();
+    }
+
+    public static List<TopicoDto> converter(List<Topico> topicos) {
+        return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(LocalDateTime dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+}
