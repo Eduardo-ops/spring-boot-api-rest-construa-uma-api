171c461d2d3950a6be81e5a4c4e704f16c5eea59
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2022-08-15 10:30:37 -0300

    Adicionado classes modelos para as entidades do projeto

---
+++ src/main/java/br/com/alura/forum/model/StatusTopicos.java
@@ -0,0 +1,9 @@
+package br.com.alura.forum.model;
+
+public enum StatusTopicos {
+
+    NAO_RESPONDIDO,
+    NAO_SOLUCIONADO,
+    SOLUCIONADO,
+    FECHADO;
+}
---
+++ src/main/java/br/com/alura/forum/model/Usuario.java
@@ -0,0 +1,76 @@
+package br.com.alura.forum.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+
+    // FechType para carregar todos os perfis do usuário, lazy loading não faz esse serviço.
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Perfil> perfis;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Perfil> getPerfis() {
+        return perfis;
+    }
+
+    public void setPerfis(List<Perfil> perfis) {
+        this.perfis = perfis;
+    }
+}
