e5aefa2bc0b8828e00445e0b74d0ac91424fac5f
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2022-08-27 14:29:28 -0300

    Adicionado autenticacao via Token JWT

--- src/main/java/br/com/alura/forum/conf/security/TokenService.java
+++ src/main/java/br/com/alura/forum/conf/security/TokenService.java
@@ -1,6 +1,7 @@
 package br.com.alura.forum.conf.security;
 
 import br.com.alura.forum.model.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,5 +32,21 @@ public class TokenService {
                 .setExpiration(dataExpiracao)
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
+    }
+
+    public boolean isTokenValido(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Long getIdUsuario(String token) {
+        Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+
+        // Pegando o id do usuário que está setado no token gerado
+        return Long.parseLong(claims.getSubject());
     }
 }
